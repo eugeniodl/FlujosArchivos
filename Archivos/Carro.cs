572fcc7d757c84e3df9536abf9393fdb177de1c6
Eugenio Díaz <Eugenio.Diaz@fcys.uni.edu.ni>
2022-03-07 18:36:05 -0600

    Archivos

---
+++ Archivos/Carro.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Archivos
+{
+    class Carro
+    {
+        private double costo;
+        private string modelo;
+
+        public double Costo { get => costo; set => costo = value; }
+        public string Modelo { get => modelo; set => modelo = value; }
+
+        // Constructor
+        public Carro(string modelo, double costo)
+        {
+            // Inicializamos los datos
+            this.costo = costo;
+            this.modelo = modelo;
+        }
+
+        public void MuestraInformacion()
+        {
+            // Mostramos la información necesaria
+            Console.WriteLine("Tu carro {0}", modelo);
+            Console.WriteLine("Costo {0}", costo);
+            Console.WriteLine("----------");
+        }
+    }
+}
---
+++ Archivos/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.IO;
+
+namespace Archivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcion = 0;
+            string valor = "";
+
+            Console.WriteLine("1) crear archivo, 2) leer archivo");
+            valor = Console.ReadLine();
+            opcion = Convert.ToInt32(valor);
+
+            if(opcion == 1)
+            {
+                // Creamos el objeto Carro
+                string modelo = "";
+                double costo = 0;
+
+                Console.WriteLine("Dame el modelo");
+                modelo = Console.ReadLine();
+
+                Console.WriteLine("Dame el costo");
+                valor = Console.ReadLine();
+                costo = Convert.ToDouble(valor);
+
+                Carro miCarro = new Carro(modelo, costo);
+
+                // Variables extras
+                int numero = 5;
+                bool acceso = false;
+                byte conteo = 120;
+
+                // Creamos el stream
+                FileStream fs = new FileStream("MiArchivo.arc", FileMode.Create, FileAccess.Write, FileShare.None);
+
+                // Creamos el escritor
+                BinaryWriter writer = new BinaryWriter(fs);
+
+                writer.Write(miCarro.Modelo);
+                writer.Write(miCarro.Costo);
+
+                // Escribimos las variables
+                writer.Write(numero);
+                writer.Write(acceso);
+                writer.Write(conteo);
+
+                // Cerramos el stream
+                fs.Close();
+            }
+
+            if(opcion == 2)
+            {
+                // Leemos el archivo
+                Console.WriteLine("--- Leemos archivo ---");
+
+                // Creamos stream
+                FileStream fs = new FileStream("MiArchivo.arc", FileMode.Open, FileAccess.Read, FileShare.None);
+
+                // Creamos el lector
+                BinaryReader lector = new BinaryReader(fs);
+
+                // Leemos en el mismo orden que se escribió
+                // tomando en cuenta el tipo
+                string modelo = "";
+                double costo = 0;
+                int numero = 0;
+                bool acceso = true;
+                byte conteo = 0;
+
+                modelo = lector.ReadString();
+                costo = lector.ReadDouble();
+                Carro miCarro = new Carro(modelo, costo);
+
+                numero = lector.ReadInt32();
+                acceso = lector.ReadBoolean();
+                conteo = lector.ReadByte();
+
+                // Cerramos el stream
+                fs.Close();
+
+                // Imprimimos
+                miCarro.MuestraInformacion();
+                Console.WriteLine("número {0}", numero);
+                Console.WriteLine("acceso {0}", acceso);
+                Console.WriteLine("conteo {0}", conteo);
+            }
+        }
+    }
+}
