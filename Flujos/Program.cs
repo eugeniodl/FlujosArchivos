e63f95c59bcc67ecabc9e3b8729b4d6e8230e4a5
Eugenio Díaz <Eugenio.Diaz@fcys.uni.edu.ni>
2022-03-07 20:47:57 -0600

    Flujos

---
+++ Flujos/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace Flujos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long cantidad = 0, n = 0;
+            int valor = 0;
+
+            FileStream fs = new FileStream("datos.txt", FileMode.Open, FileAccess.Read, FileShare.None);
+
+            // Obtenemos la cantidad
+            cantidad = fs.Length;
+            Console.WriteLine("El archivo mide {0}", cantidad);
+
+            // Leemos byte por byte
+            for (n = 0; n < cantidad; n++)
+            {
+                // Ponemos la posición
+                fs.Seek(n, SeekOrigin.Begin);
+                valor = fs.ReadByte();
+                Console.WriteLine(" {0}", (char)valor);
+            }
+        }
+    }
+}
